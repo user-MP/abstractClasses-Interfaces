84dd34fa9ed02ec61ccb888a161cf4cdf7a6c15b
user-MP <119966448+user-MP@users.noreply.github.com>
2023-01-13 20:20:40 +0200

    Update ServerNotify.java

--- AbstractTask/src/main/java/org/example/interfaces/ServerNotify.java
+++ AbstractTask/src/main/java/org/example/interfaces/ServerNotify.java
@@ -3,7 +3,7 @@ package org.example.interfaces;
 import java.time.LocalDateTime;
 
 public interface ServerNotify {
-    void setCurrentDateWithTime(int day, int month, int year, int hour, int        minute, int second);
+    void setCurrentDateWithTime(int day, int month, int year, int hour, int minute, int second);
     LocalDateTime getLastDateNotify();
     LocalDateTime getCurrentDateWithTime();
 
